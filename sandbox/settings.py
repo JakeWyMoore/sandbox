43b214fb570d5a1669fe1776e2e5eedbd4f7e943
Jacob Moore <jakewymoore@gmail.com>
2022-02-03 00:34:45 -0700

    virtual environment errors

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -125,10 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -136,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
